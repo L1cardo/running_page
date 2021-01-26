cbab80a46d105bb2c3301c3547b866b054af2b55
Licardo <1014660822@qq.com>
2021-01-26 10:55:16 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoibGljYXJkbyIsImEiOiJja2tkYWJ1bDQwOGhlMnVvNWtqMzBhcG55In0.5AtikAuJuWC4lP6dJvS6jw';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
@@ -45,10 +45,10 @@ export {
 };
 
 export const AVATAR =
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU'; // Temp avatar
+  'https://cdn.jsdelivr.net/gh/L1cardo/l1cardo.github.io@master/img/avatar.png'; // Temp avatar
 export const NAVS = [
-  { text: 'Blog', link: 'https://github.com/yihong0618/running_page' },
-  { text: 'About', link: 'https://github.com/yihong0618/running_page' },
+  { text: 'Blog', link: 'https://blog.licardo.cn' },
+  { text: 'About', link: 'https://github.com/L1cardo/running_page' },
 ];
 
 const nike = 'rgb(224,237,94)';
